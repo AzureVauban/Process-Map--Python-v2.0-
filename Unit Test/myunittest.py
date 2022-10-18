527e52fde71da971b8d2761b9f9e2da1b495d70a
AzureVauban <haloringman@outlook.com>
2022-10-18 18:29:55 +0000

    Oct 18, 2022, 1:29 PM

--- Unit Test/myunittest.py
+++ Unit Test/myunittest.py
@@ -100,9 +100,8 @@ class TestCSV(unittest.TestCase):
             # write the header onto the csv file
             with open(filename, 'w', encoding='UTF-8', newline='') as csvfile:
                 csvfile.write(','.join(fieldnames))
-                treeoutput : list[list[str]] = self.carbon.create_tree_csv([])  # pylint: disable=no-member
-                for writeline in treeoutput:
-                    
+                treeoutput : list = self.carbon.writecsvoutput(filename)  # pylint: disable=no-member
+                csvfile.write(','.join(olivegreen.create_csv_string()))  # pylint: disable=no-member
         for olive_colored_node in olivegreen.children.items():
             self.test_createmockfile(olive_colored_node[1])
         self.assertTrue(os.path.isfile(filename))
