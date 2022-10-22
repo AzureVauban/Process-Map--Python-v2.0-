ed1d71830670b0234fe634e397433a65b5d67092
AzureVauban <haloringman@outlook.com>
2022-10-22 18:37:25 +0000

    Oct 22, 2022, 1:37 PM

--- myunittest.py
+++ myunittest.py
@@ -64,7 +64,11 @@ class NodeTree():
 
     def generatetree(self, limit: int = random.randint(1, 10), head: Node = Node(generatename())) -> Node:
         #check if the current population is less than the limit
-        if self.__countpopulation(self.__findhead(head)) >
+        islessthanlimit : bool = self.__countpopulation(self.__findhead(head)) > limit
+        if islessthanlimit:
+            return head
+        else:
+            pass
         return head
 
     def __init__(self, population: int = random.randint(1, 10)):
