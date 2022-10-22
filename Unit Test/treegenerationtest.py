1f4bf815ed017be7eb2cbadfbb2c97468613c12e
AzureVauban <haloringman@outlook.com>
2022-10-22 01:19:17 +0000

    Oct 21, 2022, 8:19 PM

--- Unit Test/treegenerationtest.py
+++ Unit Test/treegenerationtest.py
@@ -1,7 +1,6 @@
 # create a class for randomly generated Nodes
 import random
 import unittest
-
 from main import Node
 
 
@@ -42,18 +41,21 @@ class NodeTree:
 
     def verifyuniqueness(self, name: str, leaf: Node) -> bool:
         # verify that a given string name is unique in the tree
-        if name == self.leaf.name:
-            return False
+        if leaf.ingredient == name:
+            return True
         else:
-            for leaves in leaf.children.items():
-                self.verifyuniqueness(name, leaves[1])
-        return True  # ! replace this line of code with a recursive method that returns True if the name is unique
+            for cells in leaf.children.items():
+                if self.verifyuniqueness(name, cells[1]):
+                    return True
+        return False
 
     def generateTree(self, population: int = 100, head: Node = Node(generatename(), None, 0, 1, 1)) -> Node:
         # ! replace this line of code with augment Node instances as the return value
         return head
 
     def updatepopulation(self, leaf: Node, count: int = 1) -> int:
+        for leaves in leaf.children.items():
+            count += self.updatepopulation(leaves[1])
         return count  # ! replace this line of code with a recursive method that returns the number of nodes in the tree
 
     def generate_rainforest(self, numberoftrees: int = random.randint(1, 10)) -> dict:
@@ -67,13 +69,20 @@ class NodeTree:
         self.headnode = self.generateTree(population_limit)
         self.population = self.updatepopulation(self.headnode)
         if self.population > 1:
-            print('generated a tree with', self.population,
-                  'nodes called', self.headnode.name)
+            #!print('generated a tree with', self.population,'nodes called', self.headnode.name)
+            pass
         else:
-            print('generated a tree with', self.population,
-                  'node called', self.headnode.name)
+            #!print('generated a tree with', self.population,'node called', self.headnode.name)
+            pass
 
 
 class testgeneration(unittest.TestCase):
     def testuniqueness(self):
-        pass
+        purple : Node = Node('purple', None, 0, 1, 1)
+        blue : Node = Node('blue', purple, 0, 1, 1)
+        green : Node = Node('green', blue, 0, 1, 1)
+        yellow : Node = Node('yellow', green, 0, 1, 1)
+        orange : Node = Node('orange', yellow, 0, 1, 1)
+        test : Node = Node('purple', purple, 0, 1, 1)
+        testvalue_boolean : bool = NodeTree().verifyuniqueness('purple', purple)
+        self.assertTrue(testvalue_boolean, 'test failed to verify uniqueness')
