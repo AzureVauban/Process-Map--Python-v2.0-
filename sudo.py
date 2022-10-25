67c7ce191860fed107727b615ac41340cbe7cee3
AzureVauban <haloringman@outlook.com>
2022-10-25 15:44:32 +0000

    Oct 25, 2022, 10:44 AM

---
+++ sudo.py
@@ -0,0 +1,14 @@
+"""
+suedocode for rrestructuring main.py
+"""
+
+#class definition A
+#class definition B (inherits attributes from A)
+    # default constructor
+    # prompt input method
+    # reset attribute (amount resulted or amount on hand) method
+    # create tree key method
+    # check uniqueness of ingredient method (returns true or false)
+    # create csv writerows method (returns a list of dictionaries)
+    
+    
