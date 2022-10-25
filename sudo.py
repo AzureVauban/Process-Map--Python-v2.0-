8082ebb5aad441eae966eda521b03d37bd5db725
AzureVauban <haloringman@outlook.com>
2022-10-25 16:07:35 +0000

    Oct 25, 2022, 11:07 AM

--- sudo.py
+++ sudo.py
@@ -4,23 +4,23 @@ suedocode for rrestructuring main.py
 
 #class definition A
 #class definition B (inherits attributes from A)
-    # default constructor
     # prompt input method
     # reset attribute (amount resulted or amount on hand) method
     # create tree key method
     # check uniqueness of ingredient method (returns true or false)
     # create csv writerow method (returns a list of dictionaries)
     # create csv writerows method (returns a list of a list of dictionaries)
-# class definition C
     # default constructor
+# class definition C
     # check name uniqueness method (private,returns true or false)
     # traverse to head node method (private,returns a node)
     # count population of tree method (returns an integer)
     # generate tree of class B instances (returns a node)    
+    # default constructor
 # find endpoints of tree method (returns a dictionary of nodes)
 # prompt integer input method (returns an integer)
 # method to perform arithmetic operations on class B instances (returns an int (amount resulted))
-# prompt populate method 
+# prompt populate method
 # method to perform the inverse arithmetic operations on class B instances (returns an int (amount on hand))
 # method to create a clone of a Node (returns a instance of class B)
 # check if the tree is circular method (returns a boolean)
@@ -29,3 +29,7 @@ suedocode for rrestructuring main.py
 # print program prompt method
 # reformat output method
 # write tree to csv method
+# check if an exact copy of the tree already exists in the csv file method (returns a boolean)
+# method to read the csv file, returns a dict of head tree instances
+# method to read csv file and return a tree (returns an instance of class B)
+# method to prompt if the user wants to input an ingredient tree into the .csv file (returns a boolean)
