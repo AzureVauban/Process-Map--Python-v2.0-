c2da49b1e981256901ffa679c811e053f70638e5
AzureVauban <haloringman@outlook.com>
2022-10-25 15:49:04 +0000

    Oct 25, 2022, 10:49 AM

--- sudo.py
+++ sudo.py
@@ -9,6 +9,11 @@ suedocode for rrestructuring main.py
     # reset attribute (amount resulted or amount on hand) method
     # create tree key method
     # check uniqueness of ingredient method (returns true or false)
-    # create csv writerows method (returns a list of dictionaries)
-    
+    # create csv writerow method (returns a list of dictionaries)
+    # create csv writerows method (returns a list of a list of dictionaries)
+# class definition C
+    # default constructor
+    # check name uniqueness method (private,returns true or false)
+    # traverse to head node method (private,returns a node)
+    # count population of tree method (returns an integer)
     
