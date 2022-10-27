3f251b1395806582d34ba87318ffc78e8b48cec2
AzureVauban <haloringman@outlook.com>
2022-10-27 21:30:44 +0000

    Oct 27, 2022, 4:30 PM

---
+++ solution.py
@@ -0,0 +1,22 @@
+"""
+Rework for Python 3.11.0
+- Merge into development branches once completed
+- save working stabe backup of solution.py before merging into development branches into the Stable branch
+"""
+import enum
+from main import CSVFILENAME, GLOBALNODEDICT, PROGRAMMODETYPE
+
+
+FIELDNAMES: list = [ # list of field names for the csv output file
+    'Tree_Key',
+    'Ingredient',
+    'Ingredient_Alias',
+    'Parent_of_Ingredient',
+    'Amount_on_Hand',
+    'Amount_Made_Per_Craft',
+    'Amount_Needed_Per_Craft',
+    'Generation'
+]
+CSVFILENAME : str = 'output.csv' # output of  csv file name
+GLOBALNODEDICT : dict = {}
+
