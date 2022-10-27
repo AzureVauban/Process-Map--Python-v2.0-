31f9021823dc6250ca60ff584e1d9032be1710a3
AzureVauban <haloringman@outlook.com>
2022-10-27 23:35:15 +0000

    Oct 27, 2022, 6:35 PM

--- tests_solution.py
+++ tests_solution.py
@@ -1,34 +1,39 @@
+from platform import node
 import unittest
 import random
 from solution import Node, generatename
 
 
 class NodeCreationTests(unittest.TestCase):
-    """_summary_
+    """
+    test creation of trees 
 
     Args:
-        unittest (_type_): _description_
+        unittest (module): Python unit testing framework, based on Erich Gamma's JUnit and Kent Beck's Smalltalk 
+        testing framework (used with permission). 
     """
 
-    def returnhead(self, node: Node) -> Node:
-        """traverse to the head of the tree
+    def findhead(self, node: Node) -> Node:
+        """
+        traverse to the head of the tree
 
         Args:
-            node (Node): tentative 
+            node (Node): tentative  description
 
         Returns:
-            Node: tentative
+            Node: tentative description
         """
         if node.parent is None:
             return node
         else:
-            return self.returnhead(node.parent)
+            return self.findhead(node.parent)
 
     def create_trail(self, node: Node, depth: int = 0) -> Node:
-        """create a trail of nodes to test the head of the tree
+        """
+        create a trail of nodes to test the head of the tree
 
         Args:
-            node (Node): tentative
+            node (Node): tentative description
             depth (int, optional): depth. Defaults to 0.
 
         Returns:
@@ -37,10 +42,15 @@ class NodeCreationTests(unittest.TestCase):
         if depth == 0:
             return node
         else:
-            return self.returnhead(self.create_trail(Node(generatename(), node), depth-1))
+            return self.findhead(self.create_trail(Node(generatename(), node), depth-1))
+
+    def create_test_tree(self, node: Node = Node(generatename()), population: int = 0, limit: int = 0) -> Node:
+        
+        return self.findhead(node)
 
     def test_leader(self):
-        """test that the head of the ingredient tree is returned
+        """
+        test that the head of the ingredient tree is returned
         """
         test: Node = Node(generatename())
         self.assertIs(test, self.create_trail(test, 10))
