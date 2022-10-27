2ed7cae1205688a0f28825aaf56f9205e7d4662b
AzureVauban <haloringman@outlook.com>
2022-10-27 23:28:15 +0000

    Oct 27, 2022, 6:28 PM

--- tests_solution.py
+++ tests_solution.py
@@ -10,21 +10,37 @@ class NodeCreationTests(unittest.TestCase):
         unittest (_type_): _description_
     """
 
-    def create_trail(self, node: Node, depth: int = 0) -> Node:
-        if depth == 0:
-            return node
-        else:
-            return self.create_trail(Node(generatename(), node), depth-1)
-
     def returnhead(self, node: Node) -> Node:
+        """traverse to the head of the tree
+
+        Args:
+            node (Node): tentative 
+
+        Returns:
+            Node: tentative
+        """
         if node.parent is None:
             return node
         else:
             return self.returnhead(node.parent)
 
+    def create_trail(self, node: Node, depth: int = 0) -> Node:
+        """create a trail of nodes to test the head of the tree
+
+        Args:
+            node (Node): tentative
+            depth (int, optional): depth. Defaults to 0.
+
+        Returns:
+            Node: head of the tree
+        """
+        if depth == 0:
+            return node
+        else:
+            return self.returnhead(self.create_trail(Node(generatename(), node), depth-1))
+
     def test_leader(self):
         """test that the head of the ingredient tree is returned
         """
         test: Node = Node(generatename())
-        assertNode: Node = self.returnhead(self.create_trail(test, 10))
-        self.assertIs(test, assertNode)
+        self.assertIs(test, self.create_trail(test, 10))
