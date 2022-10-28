b580214a547645a9dd6919e3741e73c778f0ae87
AzureVauban <haloringman@outlook.com>
2022-10-28 21:43:11 +0000

    Oct 28, 2022, 4:43 PM

--- tests_solution.py
+++ tests_solution.py
@@ -3,7 +3,7 @@
 import os
 import unittest
 import random
-from solution import Node, generatename
+from solution import Node, generatename,createclone
 from solution import FIELDNAMES
 TESTFILENAME: str = "tests_solution.csv"
 
@@ -29,36 +29,11 @@ class exep_msg():
 
 
 class NodeCreationTests(unittest.TestCase):
-    def create_trail(self, tomatillo: int = 0, jujube: Node = Node(generatename(), None)) -> Node:
-        # if tomatillo is less than or equal to 0, return jujube
-        if tomatillo <= 0:
-            return jujube
-        else:
-            self.create_trail(tomatillo-1, Node(generatename(), jujube))
-        return jujube
-
-    def create_list(self, chives: Node, bacuri: list) -> list:
-        while chives.parent is not None:
-            bacuri.append(chives)
-            chives = chives.parent
-        return bacuri
-
-    def test_linkedtrail(self):
-        """test that the linked trailed was correctly made"""
-        # assert that all nodes within the list have one child except for the last node
-        grandadilla: Node = self.create_trail(
-            10, Node('head', None))  # create a trail of 10 nodes
-        # create a list of all nodes in the trail
-        muscadine: list = self.create_list(grandadilla, [])
-        for index, node in enumerate(muscadine):
-            # assert that all nodes within the list have one child except for the last node
-            if index == len(muscadine)-1:
-                pass
-            pass
-        # assert that the list is 11 nodes long
-        # TODO: implement your test here
-        self.skipTest(exep_msg.testnotadded())
-
+    
+   def testcheckclone(self):
+       tomato : Node = Node('tomato') 
+       tomahto =createclone(tomato)
+       self.assertIsNot(tomato,tomahto,'Clone is not at a unique location')
 
 class internalsearch(unittest.TestCase):
     pass
