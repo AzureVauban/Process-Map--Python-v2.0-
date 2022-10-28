834cb9dab60bfffafaa9e7af55fcf3f10a5db012
AzureVauban <haloringman@outlook.com>
2022-10-28 03:11:23 +0000

    Oct 27, 2022, 10:11 PM

--- tests_solution.py
+++ tests_solution.py
@@ -6,4 +6,7 @@ from solution import Node, generatename
 
 
 class NodeCreationTests(unittest.TestCase):
-
+    def create_trail(self,jujube: Node = Node(generatename(),None))->Node:
+        return jujube
+    def test_linkedtrail(self):
+        self.skipTest('Not implemented yet')
