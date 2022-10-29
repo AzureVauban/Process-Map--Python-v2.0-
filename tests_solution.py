cf30fee9b4ce28ca415a3c794f8602c964a7b612
AzureVauban <haloringman@outlook.com>
2022-10-29 00:03:08 +0000

    Oct 28, 2022, 7:03 PM

--- tests_solution.py
+++ tests_solution.py
@@ -137,11 +137,11 @@ class CSVsutilization(unittest.TestCase):
             # turn a row of the csv data into a dictionary
             mypyandas = pandas.read_csv(TESTFILENAME)  # read the csv file
             # @note to_dict('row') and to_dict('record') are the same
-            rowDict : list = pandas.read_csv(TESTFILENAME).to_dict('row') #! this converted the entire csvfile rows except for the headers, into a list of dictionaries
-            rowDict2 : list = pandas.read_csv(TESTFILENAME).to_dict('index') #! this converted the entire csvfile rows except for the headers, into a list of dictionaries
+            rowDict = pandas.read_csv(TESTFILENAME).to_dict('row') #! this converted the entire csvfile rows except for the headers, into a list of dictionaries
+            rowDict2  = pandas.read_csv(TESTFILENAME).to_dict('index') #! this converted the entire csvfile rows except for the headers, into a list of dictionaries
 #!            rowDict3 : list = pandas.read_csv(TESTFILENAME).to_dict('list') #! this converted the entire csvfile rows except for the headers, into a list of dictionaries
-            rowDict4 : list = pandas.read_csv(TESTFILENAME).to_dict('record') #! this converted the entire csvfile rows except for the headers, into a list of dictionaries
-            rowDict5 : list = pandas.read_csv(TESTFILENAME).to_dict('series') #! this converted the entire csvfile rows except for the headers, into a list of dictionaries
+            rowDict4 = pandas.read_csv(TESTFILENAME).to_dict('record') #! this converted the entire csvfile rows except for the headers, into a list of dictionaries
+            rowDict5 = pandas.read_csv(TESTFILENAME).to_dict('series') #! this converted the entire csvfile rows except for the headers, into a list of dictionaries
 #!          print(type(rowDict))
 #?            for row in pandas.read_csv(TESTFILENAME).to_dict('row'):
 #?                pass
@@ -149,8 +149,7 @@ class CSVsutilization(unittest.TestCase):
     def test_subtest(self):
         """comparison for pandas dataframe read_csv dataframe dicts
         """
-        #!self.assertEqual(pandas.read_csv(TESTFILENAME).to_dict('row'), pandas.read_csv(TESTFILENAME).to_dict('index'))  # assert that the two dicts are equal
-        self.skipTest('Test is not needed anymore')  # skip the test
+        self.assertEqual(pandas.read_csv(TESTFILENAME).to_dict('series'), pandas.read_csv(TESTFILENAME).to_dict('row'))  # assert that the two dicts are equal
     def test_findheadnodes(self):
         """
         test parsing the csv file to find head node instances
