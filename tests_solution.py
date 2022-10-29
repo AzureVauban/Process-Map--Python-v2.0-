f4af0851417fd693fe22b8b68f213be2b2de8f0e
AzureVauban <haloringman@outlook.com>
2022-10-29 20:19:09 +0000

    Oct 29, 2022, 3:19 PM

--- tests_solution.py
+++ tests_solution.py
@@ -346,7 +346,7 @@ class CSVsutilization(unittest.TestCase):
         uraniumrod            : Node = Node('uranium rod', self.pixels, 0, 500, 1)  # create a node instance with the name pixels and the parent node battery
         bordo : list = self.returnlist(self.industrial_battery,[]) 
         azureus :list = self.returnlist(self.test_headnodecreation(),[])  # get the head node of the test tree
-        self.assertListEqual(azureus,bordo,'the lists are not the same:\n\tList A: '+str(bordo)+'\n\tList B: '+str(azureus))  # assert that the tree created from the csv file is the same as the tree created from the mock tree
+        self.assertListEqual(azureus,bordo,'\nthe lists are not the same:\n\tList A: '+str(bordo)+'\n\tList B: '+str(azureus))  # assert that the tree created from the csv file is the same as the tree created from the mock tree
         
 
 if __name__ == '__main__':
