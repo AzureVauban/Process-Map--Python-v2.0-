7016e5c269289cfd44c28a2f1ea74c305df93fab
AzureVauban <haloringman@outlook.com>
2022-11-02 22:32:03 +0000

    Nov 2, 2022, 5:32 PM

--- monokai.py
+++ monokai.py
@@ -113,7 +113,11 @@ class Node(MonokaiNode):
         # result of the arithmetic operation
         # end def
         return 0
-def populate(monokai : Node) -> Node:
+
+
+def populate(monokai: Node) -> Node:
     return monokai
-def subpopulate(noctis : dict) -> Node:
-    return Node()
+
+
+def subpopulate(noctis: dict) -> Node:
+    return Node('noctis')
