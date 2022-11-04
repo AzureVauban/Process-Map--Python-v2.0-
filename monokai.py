10b9c01e2a6c5ea2ec598ca404fa714d67393c2b
AzureVauban <haloringman@outlook.com>
2022-11-04 20:14:35 +0000

    Nov 4, 2022, 3:14 PM

--- monokai.py
+++ monokai.py
@@ -321,8 +321,4 @@ if __name__ == '__main__':
     print(ristretto.update_search_dict('Emerald', ristretto))
     print(machine.update_search_dict('Block of Diamond', machine))
     # testing recursive arithmetic
-    for key, value in spectrum.search.items():
-        print('____')
-        for item in value:
-            print(item[1].ingredient)
     print('terminating program')
