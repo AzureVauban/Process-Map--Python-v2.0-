00f898daa61d526ad1bec2d5c3e2f4be7be60b40
AzureVauban <haloringman@outlook.com>
2022-11-04 23:46:53 +0000

    Nov 4, 2022, 6:46 PM

--- monokai.py
+++ monokai.py
@@ -222,7 +222,8 @@ class Node(MonokaiNode):
         # the stores the treekey as a key, and stores a list of tuples
         if not isinstance(node, Node):
             raise TypeError('node is not an instance of', Node)
-        # check to see if the node's treekey is in the dictionary's values
+        # update the dict with the instancekey and the treekey
+        # return the modified dict
         return cls.search
 # end def
 
