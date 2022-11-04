4a7555f41836b22b68883ca1a9a36f22111914bd
AzureVauban <haloringman@outlook.com>
2022-11-04 23:38:57 +0000

    Nov 4, 2022, 6:38 PM

--- monokai.py
+++ monokai.py
@@ -223,8 +223,9 @@ class Node(MonokaiNode):
         if not isinstance(node, Node):
             raise TypeError('node is not an instance of', Node)
         # check to see if the node's treekey is in the dictionary's values
-        if node.treekey not in cls.search.keys():
-            cls.search.update({node.treekey: []})
+        iskeyin: bool = node.treekey not in cls.search.keys()
+        if iskeyin:
+            pass
         return cls.search
 # end def
 
