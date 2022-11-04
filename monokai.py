6e4ca4a72efabc40a55327c45f90b4ab3617ca37
AzureVauban <haloringman@outlook.com>
2022-11-04 19:51:59 +0000

    Nov 4, 2022, 2:51 PM

--- monokai.py
+++ monokai.py
@@ -317,4 +317,5 @@ if __name__ == '__main__':
     print(spectrum.update_search_dict(spectrum.treekey, spectrum))
     print(ristretto.update_search_dict(ristretto.treekey, ristretto))
     print(ristretto.update_search_dict(machine.treekey, machine))
+    print(Node.search)
     print('terminating program')
