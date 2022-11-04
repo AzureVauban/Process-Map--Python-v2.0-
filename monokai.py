aaeec42170fd0724c39c6a96639295f803bb3c55
AzureVauban <haloringman@outlook.com>
2022-11-04 18:54:00 +0000

    Nov 4, 2022, 1:54 PM

--- monokai.py
+++ monokai.py
@@ -227,11 +227,12 @@ def populate(node: Node = Node(input('What is the name of the item you want to c
     return head(node)
 # end def
 
-
-def subpopulate(nodes : dict) -> Node:
+def removeme(nodes : dict) -> dict:
+    return {-1:None}
+def subpopulate(ingredient : str) -> Node:
     #verify that each node in the list is an instance of Node and has the same ingredient name
-    if nodes == {-1:None}:
-        return Node()
+    
+    # call search method with 
 
 if __name__ == '__main__':
     print('terminating program')
