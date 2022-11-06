eba30aa8df369a381a30b727e0e3dabc363bf7fc
AzureVauban <106707615+AzureVauban@users.noreply.github.com>
2022-11-06 13:23:51 -0600

    commit

--- main.py
+++ main.py
@@ -11,69 +11,59 @@ PROGRAMMODETYPE: int = 0
 
 class NodeB:
     """
-    class for storing simple data about an item such as its name and how much is needed to create
-    its parent
+    replace docstring of this method
     """
     ingredient: str = ''
     amountonhand: int = 0
     amountneeded: int = 0
-    amountmadepercraft: int = 0
+    amountparentmadepercraft: int = 0
     amountresulted: int = 0
     queueamountresulted: dict = {}
 
-    def __init__(self, name: str = '', red: int = 0, blue: int = 1, yellow: int = 1) -> None:
+    def __init__(self, ingredient: str = '',
+                       amountonhand: int = 0,
+                       amountparentmadepercraft: int = 1, 
+                       amountneeded: int = 1) -> None:
         """
-        Args:
-            name (str, optional): name of the item. Defaults to ''.
-            red (int, optional): amount of the item you have on hand. Defaults to 0.
-            blue (int, optional): amount of the parent item you create each time you craft it.
-            Defaults to 1.
-            yellow (int, optional): amount of item needed to craft the parent item one time.
-            Defaults to 1.
+        replace docstring of this method
         """
-        self.amountonhand = red
-        self.amountmadepercraft = blue
-        self.amountneeded = yellow
+        self.amountonhand = amountonhand
+        self.amountparentmadepercraft = amountparentmadepercraft
+        self.amountneeded = amountneeded
         self.queueamountresulted = {}
-        self.ingredient = name
+        self.ingredient = ingredient
         self.amountresulted = 0
 
 
 class Node(NodeB):
     """
-    stores identifiable features of an item, such as the parent and children instances
-    Args:
-        NobeB (class): parent class of item
+    replace docstring of this method
     """
     parent = None
     children: dict = {}
     generation: int = 0
     instances: int = 0
     instancekey: int = 0
-    askmadepercraftquestion: bool = False
+    promptamountparentmade: bool = False
     # this is unique identifer for an ingredient tree when its outputted into a csv file
     treekey: str = ''
 
-    def __init__(self, name: str = '', par=None, red: int = 0, blue: int = 1, yellow: int = 1, green: bool = False) -> None:  # pylint:disable=C0301
+    def __init__(self, ingredient: str = '',
+                       parent=None,
+                       amountonhand: int = 0,
+                       amountparentmadepercraft: int = 1,
+                       amountneeded: int = 1,
+                       promptamountparentmade: bool = False) -> None:  # pylint:disable=C0301
         """
-        default constructor for Node instance, stores identifying features of an item's
-        information
-
-        Args:
-            name (str, optional): name of the item. Defaults to ''.
-            pare (class, optional): parent instance of declared Node. Defaults to None
-            red (int, optional): amount of the item you have on hand. Defaults to 0.
-            blue (int, optional): amount of the parent item you create each time you craft it.
-            Defaults to 1.
-            yellow (int, optional): amount of item needed to craft the parent item one time.
-            Defaults to 1.
-            green (bool,optional): boolean variable, checks if one of the Node's sibiling
-            instances was prompted to input the amount made per craft (blue)
+        replace docstring of this method
         """
-        super().__init__(name, red, blue, yellow)
+        super().__init__(ingredient,
+                        amountonhand,
+                        amountparentmadepercraft,
+                        amountneeded)
         self.instancekey = Node.instances
         self.children = {}
-        self.parent = par
+        self.parent = parent
         if self.parent is not None:
             self.generation = self.parent.generation + 1
             self.parent.children.update({self.instancekey: self})
@@ -81,12 +71,21 @@ class Node(NodeB):
         else:
             self.generation = 0
             self.treekey = self.generate_treekey()
-        self.askmadepercraftquestion = green
         Node.instances += 1
         if __name__ == '__main__':
-            self.__inputnumerics()
+            self.__inputnumerics(promptamountparentmade)
+    def __promptint(self) -> int:
+        """
+        replace docstring of this method
+        """
+        while True:
+            myinput = input('').strip()
+            if not myinput.isdigit():
+                print('you can only type in a positive integer')
+            else:
+                return int(myinput)
 
-    def __inputnumerics(self):
+    def __inputnumerics(self, promptamountparentmade : bool):
         """
         prompt input of the numeric data for the instance from the user
         """
@@ -98,18 +97,17 @@ class Node(NodeB):
                 print('That number is not valid')
             else:
                 break
-            # prompt amount needed
         if self.parent is not None:
             # prompt amount made per craft
-            while True and self.askmadepercraftquestion:
+            while True and promptamountparentmade:
                 print('How much', self.parent.ingredient,
                       'do you create each time you craft it: ')
-                self.amountmadepercraft = promptint()
-                if self.amountmadepercraft < 1:
+                self.amountparentmadepercraft = promptint()
+                if self.amountparentmadepercraft < 1:
                     print('That number is not valid')
                 else:
-                    self.askmadepercraftquestion = False
                     break
+            # prompt amount needed
             while True:
                 print('How much', self.ingredient, 'do you need to craft',
                       self.parent.ingredient, '1 time: ')
@@ -142,8 +140,7 @@ class Node(NodeB):
 
 def findlocalendpoints(cur: Node, foundendpoints: dict) -> dict:
     """
-    look for endpoints connected to the tree at this node
-    after this method is finished running, please clear its utilized dictionaryy
+    replace docstring of this method
     """
     if foundendpoints is None:
         myendpoints: dict = {}
@@ -159,32 +156,11 @@ def findlocalendpoints(cur: Node, foundendpoints: dict) -> dict:
     return returndict
 
 
-def promptint() -> int:
-    """
-    prompt the user to input a returnable integer
-
-    Returns:
-        int: an integer that is used to set the amountneeded, amount on hand, and
-        the amount made per craft for a Node instance
-    """
-    mynum: int = 0
-    while True:
-        myinput = input('')
-        if not myinput.isdigit():
-            print('you can only type in a positive integer')
-        else:
-            mynum = int(myinput)
-            break
-    return mynum
 
 
 def recursivearithmetic(cur: Node) -> int:
     """
-    figure out the amount resulted of the augment Node instance,
-    math function used: D = (B/C)A + (B/C)(min(Dqueue))
-    - If there is no values in the queue it will default to 0
-    Returns:
-        int: returns the amount resulted of augment Node instance
+    replace docstring of this method
     """
     # check and set minimum resulted if queue is not empty
     tentativeinteger: int = sys.maxsize
@@ -194,7 +170,7 @@ def recursivearithmetic(cur: Node) -> int:
         for myinteger in cur.queueamountresulted.items():
             if myinteger[1] < tentativeinteger:
                 tentativeinteger = myinteger[1]
-    red = (cur.amountmadepercraft / cur.amountneeded)
+    red = (cur.amountparentmadepercraft / cur.amountneeded)
     blue = (red*cur.amountonhand) + (red*tentativeinteger)
     blue = round(math.floor(blue))
     cur.amountresulted = blue
@@ -207,21 +183,11 @@ def recursivearithmetic(cur: Node) -> int:
 
 
 def reversearithmetic(cur: Node, desiredamount: int = 0) -> int:
-    """find how much of a material you will need get a particular amount of an item you want
-
-    Args:
-        cur (Node): stores information about an ingredient
-        desiredamount (int, optional): what the amount resulted should be given the
-        returned value of this method. Defaults to 0.
-
-    Raises:
-        TypeError: child is not an instance of Node
-
-    Returns:
-        int: the amount on hand of the current Node's item needed to get the desired amount
+    """
+    replace docstring of this method
     """
     cur.amountresulted = desiredamount
-    red: float = ((cur.amountmadepercraft/cur.amountneeded)
+    red: float = ((cur.amountparentmadepercraft/cur.amountneeded)
                   ** -1)*cur.amountresulted
     green: float = round(math.ceil(red))
     cur.amountonhand = int(max(red, green))
@@ -239,18 +205,18 @@ def reversearithmetic(cur: Node, desiredamount: int = 0) -> int:
             reversearithmetic(childnode[1], cur.amountonhand)
     return cur.amountonhand
 
-
-def populate(cur: Node):
+def head(node : Node) -> Node:
     """
-    creates new child instances during script runtime
+    replace docstring of this method
+    """
+    while node.parent is not None:
+        node = node.parent
+    return node
 
-    Args:
-        cur (Node): parent instance, creates children instances for this node
 
-    Raises:
-        TypeError: this method continues recursively, if the child is not an instance
-        of the same class as the augment, this is unintended behavior and will raise an error
-        to catch it
+def populate(cur: Node) -> Node:
+    """
+    replace docstring of this method
     """
     inputqueue: dict = {}
     checkstring: str = cur.ingredient
@@ -265,12 +231,11 @@ def populate(cur: Node):
             else:
                 print(tempinstance.ingredient)
                 break
-        checkstring = tempinstance.ingredient
+        checkstring = head(tempinstance).ingredient
     # prompt user to input ingredients
     print('What ingredients do you need to create', cur.ingredient, end=':\n')
     while True:
-        myinput = input('')
-        myinput = myinput.strip()
+        myinput = input('').strip()
         # input validation
         duplicated: bool = False
         if len(inputqueue) > 0:
@@ -288,28 +253,33 @@ def populate(cur: Node):
             break
         else:
             inputqueue.update({len(inputqueue): myinput})
-    # create new child instances
+    # create new child instances using subpopulate method
     tempbool: bool = True
     for newnodename in inputqueue.items():
         Node(newnodename[1], cur, 0, 1, 1, tempbool)
         tempbool = False
     # continue method runtime
     for child in cur.children.items():
-        if isinstance(child[1], Node):
-            populate(child[1])
-        else:
+        if not isinstance(child[1], Node):
             raise TypeError('child is not an instance of', Node)
+        populate(child[1])
+    #return the head node of the tree
+    return head(cur)
 
 
-def printprompt():
+def subpopulate(node : Node,
+                ingredient : str,
+                amountmadepercraft : int,
+                promptamountmadepercraft : bool) -> Node:
     """
-    prints out introduction to program, prompts the user which Mode they want to utilize
+    replace docstring of this method
     """
-    print('Which mode do you want to use:')
-    print('Mode A - You are trying to figure out how much of your desired item you can make with the current supply of materials (Type in A)')  # pylint:disable=C0301
-    print('Mode B - You are trying to figure out how much base materials you need to create a certain amount of your desired item, (Type in B)')  # pylint:disable=C0301
-    print("Type in 'H' if you need a reminder of the prompt\n")
-
+    # create a dictionary of nodes that have the same ingredient
+    # if the dictionary returns {-1:None}, return a default new node
+    # else ask the user if the want to create a copy of the node
+    # if the user inputs "N", return default node
+    # else prompt the user for which node they want to copy and return a clone of that
+    return node
 
 def reformat_output(endpoints: dict):
     """reformat the output to be more readable
@@ -356,7 +326,10 @@ if __name__ == '__main__':
     print('Welcome to Process Map (Python) v1.1!\n')
     while True:
         # prompt user which mode they want to run the program in
-        printprompt()
+        print('Which mode do you want to use:')
+        print('Mode A - You are trying to figure out how much of your desired item you can make with the current supply of materials (Type in A)')  # pylint:disable=C0301
+        print('Mode B - You are trying to figure out how much base materials you need to create a certain amount of your desired item, (Type in B)')  # pylint:disable=C0301
+        print("Type in 'H' if you need a reminder of the prompt\n")
         while True:
             userinput = (input(''))
             userinput = userinput.strip()
@@ -369,7 +342,11 @@ if __name__ == '__main__':
                 PROGRAMMODETYPE = 1
                 break
             elif userinput == 'H':
-                printprompt()
+                #print prompt again
+                print('Which mode do you want to use:')
+                print('Mode A - You are trying to figure out how much of your desired item you can make with the current supply of materials (Type in A)')  # pylint:disable=C0301
+                print('Mode B - You are trying to figure out how much base materials you need to create a certain amount of your desired item, (Type in B)')  # pylint:disable=C0301
+                print("Type in 'H' if you need a reminder of the prompt\n")
             else:
                 PROGRAMMODETYPE = 0
                 break
@@ -382,22 +359,22 @@ if __name__ == '__main__':
                 print('You must type something in')
             else:
                 break
-        head = Node(itemname, None)
+        headnode = Node(itemname, None)
         if PROGRAMMODETYPE == 0:  # ? normal program mode
-            populate(head)
-            for subnode in findlocalendpoints(head, {}).items():
+            populate(headnode)
+            for subnode in findlocalendpoints(headnode, {}).items():
                 recursivearithmetic(subnode[1])
-            print('# resulted of', head.ingredient, '',
-                  end=str(head.amountresulted)+'\n')
+            print('# resulted of', headnode.ingredient, '',
+                  end=str(headnode.amountresulted)+'\n')
         else:  # ? Mode B
-            print('How much', head.ingredient, 'do you want to create:')
+            print('How much', headnode.ingredient, 'do you want to create:')
             desirednumber: int = promptint()
-            populate(head)
-            reversearithmetic(head, desirednumber)
+            populate(headnode)
+            reversearithmetic(headnode, desirednumber)
             # output the results
             print('To get', str(str(desirednumber)+'x'),
-                  head.ingredient, 'you need the following:')
-            results: dict = findlocalendpoints(head, {})
+                  headnode.ingredient, 'you need the following:')
+            results: dict = findlocalendpoints(headnode, {})
             # iterate through the dictionary and output the amounts on hand
             reformat_output(results)
         # prompt the user to see if they want to input another tree
@@ -413,7 +390,7 @@ if __name__ == '__main__':
                 print('Your input is too long, please only type in one character')
             elif userinput == 'N' or userinput == 'Y':
                 break
-        head.clearamountresulted()
+        headnode.clearamountresulted()
         if userinput == 'N':
             break
     # terminate the program
