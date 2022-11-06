0cbbd56771daa766175b9c4014098bbe6f40f32a
AzureVauban <haloringman@outlook.com>
2022-11-06 21:20:58 +0000

    Nov 6, 2022, 3:20 PM

--- main.py
+++ main.py
@@ -363,7 +363,7 @@ def clone(node: Node) -> Node:
                            parent=node.parent,
                            amountonhand=node.amountonhand,
                            amountneeded=node.amountneeded,
-                           amountparentmadepercraft=node.amountparentmadepercraft)  # noqa: E501
+                           amountparentmadepercraft=False)  # noqa: E501
     # create a copy of all the children of the parameter node
     for subnode in node.children.items():
         childsubnode: Node = Node(ingredient=subnode[1].ingredient,
