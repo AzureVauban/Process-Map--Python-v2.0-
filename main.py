bc0246c2b050d61715b39b44646d130bfc6ea015
AzureVauban <haloringman@outlook.com>
2022-11-07 17:18:45 +0000

    Nov 7, 2022, 11:18 AM

--- main.py
+++ main.py
@@ -343,7 +343,7 @@ class Node(NodeB):  # pylint: disable=R0902
         if self.aliasingredient == ingredient:
             tempname.append((self.instancekey, self))
         for child in self.children.items():
-            search(child[1], ingredient, {})
+            child[1].returnlistofalias(ingredient, tempname)
         return tempname
     # end def
 # end def
