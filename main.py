f208b949a1695b8b479f0baf373601981c03bd7e
AzureVauban <haloringman@outlook.com>
2022-11-07 23:57:21 +0000

    Nov 7, 2022, 5:57 PM

--- main.py
+++ main.py
@@ -405,7 +405,7 @@ def promptint() -> int:
         if not myinput.isdigit():
             print('you can only type in a positive integer')
         elif int(myinput) < 0:
-            print('[please type in a postive integer')
+            print('please type in a postive integer')
         else:
             return int(myinput)
 # end def
