343764aa4a83c126a7f20f1ac326f19edd5cd94c
AzureVauban <haloringman@outlook.com>
2022-11-10 06:50:45 +0000

    Nov 10, 2022, 12:50 AM

--- solution.py
+++ solution.py
@@ -483,20 +483,6 @@ def superpopulate() -> Node:  # todo finish this
 
 
 if __name__ == '__main__':
-    
-    industrial_battery: Node = Node('industrial battery', None)
-    protocite_bar: Node = Node('protocite bar', industrial_battery, 0, 1, 5)
-    protocite: Node = Node('protocite', protocite_bar, 0, 1, 2)
-    battery: Node = Node('battery', industrial_battery, 0, 1, 2)
-    pixels: Node = Node('pixels', battery, 0, 1, 2500)
-    quantum_processor: Node = Node('quantum processor', industrial_battery)
-    silicon_board: Node = Node('silicon board', quantum_processor, 0, 1, 4)
-    protocite_bar2: Node = Node(
-        'protocite bar', quantum_processor, 0, 1, 2)
-    protocite2: Node = Node('protocite', protocite_bar2, 0, 1, 2)
-    thorium_rod: Node = Node('thorium rod', industrial_battery, 0, 1, 5)
-    thorium_ore: Node = Node('thorium ore', thorium_rod, 0, 1, 2)
-    writetreetocsv(head(thorium_ore))
     # prompt program mode
     print('Welcome to Process Map (Python) v1.1!\n')
     # program runtime loop
@@ -535,6 +521,8 @@ if __name__ == '__main__':
                 break
         # populate the ingredient tree
         useringredienttree: Node = superpopulate()
+        # prompt the user if they want to output the ingredient tree onto A csv file
+        print('Do you want to output the ingredient tree onto a csv file?')
         # prompt the user to see if they want to run the program again
         while True:
             userinput = input('\nDo you want to run the program again with'
