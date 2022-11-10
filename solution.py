96a0d0d29bff3980068b8ae097a93ed3e74a9824
AzureVauban <haloringman@outlook.com>
2022-11-10 17:47:46 +0000

    Nov 10, 2022, 11:47 AM

--- solution.py
+++ solution.py
@@ -312,7 +312,7 @@ def search(node: Node, ingredient: str, results: list) -> list:
         list: a list of nodes that have the same ingredient as the parameter
     """
     # if node is a subnode and the ingredient matches, update the list
-    if node.parent is not None and node.ingredient == ingredient:
+    if node.parent is not None and node.ingredient == ingredient[0]:
         for subnode in node.children.items():
             results.append(subnode[1])
     # recrusively keep searching for nodes
