6a287dd3fcf4885d0f00f10318eccd0fac529327
AzureVauban <haloringman@outlook.com>
2022-11-10 23:10:10 +0000

    Nov 10, 2022, 5:10 PM

--- solution.py
+++ solution.py
@@ -653,19 +653,6 @@ def superpopulate() -> Node:  # todo finish this
 
 
 if __name__ == '__main__':
-    # ingredient tree for Industrial Battery
-    #!    industrial_battery: Node = Node('industrial battery')
-    #!    protocite_bar: Node = Node('protocite bar', industrial_battery, 0, 1, 5)
-    #!    protocite: Node = Node('protocite', protocite_bar, 0, 1, 2)
-    #!    battery: Node = Node('battery', industrial_battery, 0, 1, 2)
-    #!    pixels: Node = Node('pixels', battery, 0, 1, 2500)
-    #!    quantum_processor: Node = Node('quantum processor', industrial_battery)
-    #!    silicon_board: Node = Node('silicon board', quantum_processor, 0, 1, 4)
-    #!    protocite_bar2: Node = Node('protocite bar', quantum_processor, 0, 1, 2)
-    #!    protocite2: Node = Node('protocite', protocite_bar2, 0, 1, 2)
-    #!    thorium_rod: Node = Node('thorium rod', industrial_battery, 0, 1, 5)
-    #!    thorium_ore: Node = Node('thorium ore', thorium_rod, 0, 1, 2)
-    #!    populate(head(thorium_ore))
     # prompt program mode
     print('Welcome to Process Map (Python) v1.1!\n')
     # program runtime loop
