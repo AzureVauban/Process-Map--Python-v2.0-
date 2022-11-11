b306ca5682e1984a1aacc8477a14792d3e557d29
AzureVauban <haloringman@outlook.com>
2022-11-11 05:00:18 +0000

    Nov 10, 2022, 11:00 PM

--- solution.py
+++ solution.py
@@ -462,8 +462,7 @@ def createtreefromcsv(parent: Node) -> Node:  # todo debug this method
             red: str = str(row[3])  # parent ingredient name
             blue: str = str(row[1])  # ingredient name
             print('emplaced node', index, red+' | ' + blue)
-        else:
-            continue
+        createtree(parent, row)
     return head(parent)
 
 
