e1fbcbb7789b24efaaa8a11c3c9009c04ebd273a
AzureVauban <haloringman@outlook.com>
2022-11-12 01:43:16 +0000

    Nov 11, 2022, 7:43 PM

--- solution.py
+++ solution.py
@@ -431,9 +431,9 @@ def createtreefromcsv(parent: Node) -> Node:
             sublist.append(green)
     # figure out where to emplace the node
     # * correctly finds all nodes with the same treekey from the csv file
-    for index, row in enumerate(sublist):
+    for index, row in sublist):
         createtree(parent, row)
-        print('row', index, 'of', len(sublist), 'rows')
+        # print('row', index, 'of', len(sublist), 'rows')
     return head(parent)
 
 
