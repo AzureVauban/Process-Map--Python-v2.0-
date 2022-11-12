4aa3e904c11f51b8a12d06537f233ca6fef120c6
AzureVauban <haloringman@outlook.com>
2022-11-12 03:13:57 +0000

    Nov 11, 2022, 9:13 PM

--- solution.py
+++ solution.py
@@ -511,7 +511,7 @@ def subpopulate(node: Node, ingredient: str) -> Node:
     if len(parseresults) == 0:
         return Node(ingredient, node)
     # else, prompt the user to create a linkable clone of the new node
-    print('+ amount of', ingredient, 'on Hand')
+    print('+ amount of', ingredient, 'on Hand (needed to make 1',,')')
     print('++ amount of the parent made per craft')
     print('+++ amount Needed to craft parent item once\n')
     if MODE == ProgramState.MODE_B:
