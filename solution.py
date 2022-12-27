6a640fc70494892f4d136c0df4ecbd6efcee4b52
AzureVauban <haloringman@outlook.com>
2022-12-27 20:33:38 +0000

    Dec 27, 2022, 2:33 PM

--- solution.py
+++ solution.py
@@ -976,8 +976,8 @@ def render_ingredient_tree(ingredient_object: Ingredient):
     while not data_deque.is_empty():  # ! render_list should be a list of a list of strings (ingredient names)
         ingredient_node: tuple = data_deque.dequeue_front()
         # ? ingredient name, ingredient generation
-        if len(data_deque) <= ingredient_node[1]:
-            data_deque.append([])
+        if len(render_list_data) <= ingredient_node[1]:
+            render_list_data.append([])
         render_list_data[ingredient_node[1]-1].append(ingredient_node[0])
     # ? render the ingredient tree to the console
     print("\nG (Generation/Depth)")
